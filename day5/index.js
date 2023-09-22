d75448c2a819161e0f39151d8c4d47696675007a
ManasRanjanBariha <manasbariha779@gmail.com>
2023-09-22 16:03:24 +0530

    adding day5 blurry animation loading

---
+++ day5/index.js
@@ -0,0 +1,20 @@
+const loadingText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+function blurring() {
+  load++;
+  if (load > 99) {
+    clearInterval(init);
+  }
+  console.log(load);
+  loadingText.innerHTML = `${load}%`;
+  loadingText.style.opacity=scale(load,0,100,1,0);
+  bg.style.filter=`blur(${scale(load,0,100,20,0)}px)`
+}
+let init = setInterval(blurring, 30);
+
+const scale = (num, in_min, in_max, out_min, out_max)=>{
+
+  return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
